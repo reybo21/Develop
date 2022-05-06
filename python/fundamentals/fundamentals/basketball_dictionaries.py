be6f13d7a08bc0c77a83291b15dfb5c9757c0465
reybo21 <rbosque88@gmail.com>
2022-05-05 20:35:59 -0500

    basket submit v2

--- python/fundamentals/fundamentals/basketball_dictionaries.py
+++ python/fundamentals/fundamentals/basketball_dictionaries.py
@@ -1,45 +1,44 @@
 players = [
     {
-    "name": "Kevin Durant", 
-    "age":34, 
-    "position": "small forward", 
-    "team": "Brooklyn Nets"
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
     },
     {
-    "name": "Jason Tatum", 
-    "age":24, 
-    "position": "small forward", 
-    "team": "Boston Celtics"
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
     },
     {
-    "name": "Kyrie Irving", 
-    "age":32, 
-    "position": "Point Guard", 
-    "team": "Brooklyn Nets"
+        "name": "Kyrie Irving", 
+        "age":32, 
+        "position": "Point Guard", 
+        "team": "Brooklyn Nets"
     },
     {
-    "name": "Damian Lillard", 
-    "age":33, 
-    "position": "Point Guard", 
-    "team": "Portland Trailblazers"
+        "name": "Damian Lillard", 
+        "age":33, 
+        "position": "Point Guard", 
+        "team": "Portland Trailblazers"
     },
     {
-    "name": "Joel Embiid", 
-    "age":32, 
-    "position": "Power Foward", 
-    "team": "Philidelphia 76ers"
+        "name": "Joel Embiid", 
+        "age":32, 
+        "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
     },
     {
-    "name": "", 
-    "age":16, 
-    "position": "P", 
-    "team": "en"
+        "name": "", 
+        "age":16, 
+        "position": "P", 
+        "team": "en"
     }
 ]
 
-class Player:
 
-    new_team = []
+class Player:
 
     def __init__(self, player_info):
         self.name = player_info["name"]
@@ -52,9 +51,10 @@ class Player:
 
     @classmethod
     def get_team(cls, team_list):
+        new_team = []
         for player in team_list:
-            cls.new_team.append(Player(player))
-        return cls.new_team
+            new_team.append(Player(player))
+        return new_team
 
 
 new_team = Player.get_team(players)
